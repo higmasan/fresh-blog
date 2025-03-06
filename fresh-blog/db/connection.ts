1885a7476b0eb54165b72ed17438522277fa7411
yoshiyasu1111 <aris_and_teres_31415926535@yahoo.co.jp>
2025-03-06 06:46:40 +0000

    データベースの変更

--- fresh-blog/db/connection.ts
+++ fresh-blog/db/connection.ts
@@ -12,7 +12,7 @@ export async function connectDB() {
     await client.connect(MONGODB_URI);
     console.log("Connected to MongoDB");
     
-    db = client.database("fresh-blog");
+    db = client.database("fresh-blog-dev-cluster");
     return db;
   } catch (error) {
     console.error("Database connection error:", error);
